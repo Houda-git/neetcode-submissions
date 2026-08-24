0d4f97fd97b06c70d39207e25f45cf33dddd46e1
Houda <houda.rachidi02@gmail.com>
2026-08-24 14:15:45 +0200

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # another approach that is less in complexity
+        difference_map ={}
+        for i in range(len(nums)):
+           difference = target - nums[i]
+           if difference in difference_map:
+            return [difference_map[difference],i]
+           difference_map[nums[i]] = i
+
+        
